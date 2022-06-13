0d8ce7c9c30b7bd8e25e911d5a9bba4538251b58
Freddy <drennanfreddy@gmail.com>
2022-06-13 13:58:19 -0600

    save

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <BrowserRouter>
+    <div>Hello</div>
+    {/*<Routes>*/}
+    {/*  <Route path="/" element={<App />} />*/}
+    {/*  <Route path="expenses" element={<Expenses />} />*/}
+    {/*  <Route path="invoices" element={<Invoices />} />*/}
+    {/*</Routes>*/}
+  // </BrowserRouter>
+  // <App />
+);
