4a01efa781d6ca16c9f6c114da7ff5e35c1cc533
Freddy <drennanfreddy@gmail.com>
2022-06-13 18:55:31 -0600

    save

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./routes/Navbar/Navbar";
 import App from "./routes/App/App";
 import Settings from "./routes/Settings/Settings";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="settings" element={<Settings />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="settings" element={<Settings />} />
+    </Routes>
+  </BrowserRouter>
 );
